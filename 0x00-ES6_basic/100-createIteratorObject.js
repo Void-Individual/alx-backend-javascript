70ca2a1c6020c82d991a525150766849a9e92b03
Void-Individual <otayomitv@gmail.com>
2024-06-26 13:23:08 +0100

    advanced tasks

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,12 @@
 export default function createIteratorObject(report) {
+  const employees = [];
+  const { allEmployees } = report;
 
+  for (const dept of Object.keys(allEmployees)) {
+    for (const employee of allEmployees[dept]) {
+      employees.push(employee);
+    }
+  }
+
+  return employees;
 }
--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,13 @@
 export default function iterateThroughObject(reportWithIterator) {
-
+  const len = reportWithIterator.length;
+  let x = 0;
+  let names = '';
+  for (const name of reportWithIterator) {
+    names += name;
+    if (x < len - 1) {
+      names += ' | ';
+    }
+    x += 1;
+  }
+  return names;
 }
