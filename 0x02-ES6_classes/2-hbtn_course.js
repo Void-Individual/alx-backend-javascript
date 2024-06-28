60cba67949e1c1b0fc40aedbf2481cce314b09e9
Void-Individual <otayomitv@gmail.com>
2024-06-28 19:42:11 +0100

    all done

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,8 @@
+export default class HolbertonCourse {
+/*eslint-disable-line no-underscore-dangle*/
+  constructor(name, length, students) {
+    if (typeof name === st
+
+  }
+/*eslint-enable-line no-underscore-dangle*/
+}
