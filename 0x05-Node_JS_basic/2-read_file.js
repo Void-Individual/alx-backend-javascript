406aad8bbc229e2cad3c0151b0b1324f3e995408
Void-Individual <otayomitv@gmail.com>
2024-08-20 23:55:17 +0100

    Checking task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,30 +1,36 @@
 const fs = require('fs');
 
 module.exports = function countStudents(path) {
-  try {
-    const data = fs.readFileSync(path, 'utf-8');
-    const lines = data.split('\n');
-    const fields = {};
-    let count = 0;
-    for (let x = 1; x < lines.length; x += 1) {
-      if (lines[x].length > 0) {
-        count += 1;
-        const row = lines[x].split(',');
-
-        if (!(row[3] in fields)) {
-          fields[row[3]] = [1, [row[0]]];
-        } else {
-          fields[row[3]][0] += 1;
-          fields[row[3]][1].push(row[0]);
-        }
-      }
-    }
-      console.log('Number of students:', count);
-      Object.keys(fields).forEach((key) => {
-        process.stdout.write(`Number of students in ${key}: ${fields[key][0]} `);
-        process.stdout.write(`List: ${fields[key][1].join(', ')}\n`);
-      });
-  } catch (err) {
+  if (!fs.existsSync(path)) {
     throw new Error('Cannot load the database');
   }
+  if (!fs.statSync(path).isFile()) {
+    throw new Error('Cannot load the database');
+  }
+
+  const lines = fs
+    .readFileSync(path, 'utf-8')
+    .toString('utf-8')
+    .trim()
+    .split('\n');
+  const fields = {};
+  let count = 0;
+  for (let x = 1; x < lines.length; x += 1) {
+    if (lines[x].length > 0) {
+      count += 1;
+      const row = lines[x].split(',');
+
+      if (!(row[3] in fields)) {
+        fields[row[3]] = [1, [row[0]]];
+      } else {
+        fields[row[3]][0] += 1;
+        fields[row[3]][1].push(row[0]);
+      }
+    }
+  }
+  console.log('Number of students:', count);
+  Object.keys(fields).forEach((key) => {
+    process.stdout.write(`Number of students in ${key}: ${fields[key][0]} `);
+    process.stdout.write(`List: ${fields[key][1].join(', ')}\n`);
+  });
 };
