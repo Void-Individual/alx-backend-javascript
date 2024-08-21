ed1f378943a377ab7a35fb270b785a848fabe4ad
Void-Individual <otayomitv@gmail.com>
2024-08-21 01:59:19 +0100

    Reduced line length

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -21,7 +21,8 @@ async function countStudents(path) {
     }
     console.log('Number of students:', count);
     Object.keys(fields).forEach((key) => {
-      process.stdout.write(`Number of students in ${key}: ${fields[key][0]}. `);
+      const count = fields[key][0];
+      process.stdout.write(`Number of students in ${key}: ${count}. `);
       process.stdout.write(`List: ${fields[key][1].join(', ')}\n`);
     });
   } catch (err) {
