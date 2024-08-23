5f5270fe8cd8bf1812ab1b118ae6416818412b23
Void-Individual <otayomitv@gmail.com>
2024-08-24 00:39:19 +0100

    reverted 3

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,8 +1,8 @@
-const Utils = require('./utils');
+const Utils  = require('utils');
 
 const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
-  const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is: ${totalCost}`);
-};
+  const res = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total amount is: ${res}`);
+}
 
 module.exports = sendPaymentRequestToApi;
