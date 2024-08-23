290bb8ee7054769c4015113acd6a1de1526b7f34
Void-Individual <otayomitv@gmail.com>
2024-08-24 00:41:18 +0100

    Now, reverted test

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,8 +1,8 @@
-const Utils  = require('utils');
+const Utils = require('./utils');
 
 const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
-  const res = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total amount is: ${res}`);
-}
+  const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${totalCost}`);
+};
 
 module.exports = sendPaymentRequestToApi;
--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,15 +1,19 @@
 const sinon = require('sinon');
-const Utils = require('./utils');
 const { expect } = require('chai');
+const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
-  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
-    const bigBrother = sinon.spy(Utils);
+  it(`sendPaymentRequestToApi(100, 20) should return the same as \
+   Utils.calculateNumber(SUM, 100,20)`, () => {
+    const utilsSpy = sinon.spy(Utils);
 
     sendPaymentRequestToApi(100, 20);
-    expect(bigBrother.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
-    expect(bigBrother.calculateNumber.callCount).to.be.equal(1);
-    bigBrother.calculateNumber.restore();
+
+    // Validate that Utils.calculateNumber was called with the correct arguments
+    expect(utilsSpy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(utilsSpy.calculateNumber.callCount).to.be.equal(1);
+
+    utilsSpy.calculateNumber.restore();
   });
 });
