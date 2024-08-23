f37a78015691c81370613b07d0002ea278e57ab6
Void-Individual <otayomitv@gmail.com>
2024-08-24 00:42:47 +0100

    Test to original

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -3,17 +3,16 @@ const { expect } = require('chai');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./3-payment');
 
-describe('sendPaymentRequestToApi', () => {
+describe('sendPaymentRequestToApi', function() {
   it(`sendPaymentRequestToApi(100, 20) should return the same as \
-   Utils.calculateNumber(SUM, 100,20)`, () => {
-    const utilsSpy = sinon.spy(Utils);
+   Utils.calculateNumber(SUM, 100,20)`, function() {
+    const utilsSpy = sinon.spy(Utils, 'calculateNumber');
 
     sendPaymentRequestToApi(100, 20);
 
     // Validate that Utils.calculateNumber was called with the correct arguments
-    expect(utilsSpy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
-    expect(utilsSpy.calculateNumber.callCount).to.be.equal(1);
+    expect(utilsSpy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
 
-    utilsSpy.calculateNumber.restore();
+    utilsSpy.restore();
   });
 });
