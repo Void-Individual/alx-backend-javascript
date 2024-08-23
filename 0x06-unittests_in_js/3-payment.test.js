8cf25142e3348a1b08e081e95bdce4527f0a29d4
Void-Individual <otayomitv@gmail.com>
2024-08-23 04:33:01 +0100

    Run spies

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,18 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./sendPaymentRequestToApi');
+
+describe('sendPaymentRequestToApi', function() {
+  it(`sendPaymentRequestToApi(100, 20) should return the same as \
+   Utils.calculateNumber(SUM, 100,20)`, function() {
+    const utilsSpy = sinon.spy(Utils, 'calculateNumber');
+
+    sendPaymentRequestToApi(100, 20);
+
+    // Validate that Utils.calculateNumber was called with the correct arguments
+    expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+
+    utilsSpy.restore();
+  });
+});
