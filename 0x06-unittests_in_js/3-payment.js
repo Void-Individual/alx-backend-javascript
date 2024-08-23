ebbc80de950ce6903cc7ebfcd7c64393e7f80c62
Void-Individual <otayomitv@gmail.com>
2024-08-24 00:33:03 +0100

    Changed payment line

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,6 +1,6 @@
 const Utils  = require('utils');
 
-function sendPaymentRequestToApi(totalAmount, totalShipping) {
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
   const res = Utils.calculateNumber('SUM', totalAmount, totalShipping);
   console.log(`The total amount is: ${res}`);
 }
