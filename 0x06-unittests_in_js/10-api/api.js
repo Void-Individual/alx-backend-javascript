d26e00bd3e779d97a650dff60a1d004ccf0ceee8
Void-Individual <otayomitv@gmail.com>
2024-08-24 01:55:52 +0100

    hardopee

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const id = req.params.id;
+
+  res.send(`Payment methods for cart ${id}`);
+});
+
+app.get('/available_payments', (_req, res) => {
+  res.json({ payment_methods: { credit_cards: true, paypal: false } });
+});
+
+app.post('/login', (req, res) => {
+  let username = '';
+
+  if (req.body) {
+    username = req.body.userName;
+  }
+
+  res.send(`Welcome ${username}`);
+});
+
+const port = 7865;
+app.listen(port, () => {
+  console.log(`API available on localhost port 7865`);
+});
+
+module.exports = app;
